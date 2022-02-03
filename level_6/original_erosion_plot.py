4c147b2a8066d6963d730447c41f9f45b9e16153
SDY159 <98853220+SDY159@users.noreply.github.com>
2022-02-02 16:16:39 -0800

    Add files via upload

---
+++ level_6/original_erosion_plot.py
@@ -0,0 +1,103 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import matplotlib.cm as cmx
+import matplotlib
+import matplotlib.ticker as ticker
+def data_for_cylinder_main(center_y,center_z,radius,height_x):
+       x = np.linspace(0, height_x, 50)-0.64025
+       theta = np.linspace(0, 2*np.pi, 50)
+       theta_grid, x_grid=np.meshgrid(theta, x)
+       y_grid = radius*np.cos(theta_grid) + center_y
+       z_grid = radius*np.sin(theta_grid) + center_z
+       return x_grid,y_grid,z_grid
+Xm,Ym,Zm = data_for_cylinder_main(0,0,0.269,1.8805)
+def data_for_cylinder_inlet(center_x,center_z,radius,height_y):
+       y = np.linspace(0, -height_y, 50)-0.269
+       theta = np.linspace(0, 2*np.pi, 50)
+       theta_grid, y_grid=np.meshgrid(theta, y)
+       x_grid = radius*np.cos(theta_grid) + center_x
+       z_grid = radius*np.sin(theta_grid) + center_z
+       return x_grid,y_grid,z_grid
+Xi1,Yi1,Zi1 = data_for_cylinder_inlet(-0.5435,0,0.01875,0.269)
+Xi2,Yi2,Zi2 = data_for_cylinder_inlet(0.0,0,0.01875,0.269)
+Xi3,Yi3,Zi3 = data_for_cylinder_inlet(0.5435,0,0.01875,0.269)
+
+sim_num = 2360
+
+path = ('/bigdata/wonglab/syang159/OP650_1/')
+os.chdir(path + 'cell_%i/'%sim_num)
+import pandas as pd
+df = pd.read_csv("corrosion_pressure")
+X = df.iloc[:,1].values
+Y = df.iloc[:,2].values
+Z = df.iloc[:,3].values
+W = df["dpm-erosion-rate-finnie"].values
+fig = plt.figure()
+
+ax=fig.add_subplot(111,projection='3d')
+fig.set_size_inches(18.5, 10.5)
+
+ax.plot_surface(Xm,Zm,Ym,alpha=0.1,color='blue')
+ax.plot_surface(Xi1,Zi1,Yi1,alpha=0.1,color='blue')
+ax.plot_surface(Xi2,Zi2,Yi2,alpha=0.1,color='blue')
+ax.plot_surface(Xi3,Zi3,Yi3,alpha=0.1,color='blue')
+
+
+
+
+cm = plt.get_cmap('rainbow')
+cNorm = matplotlib.colors.Normalize(vmin=min(W), vmax=max(W))
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+# ax.set_xlabel('X-coordinate (m)', fontsize=12)
+# ax.set_ylabel('Y-coordinate (m)', fontsize=12)
+# ax.set_zlabel('Z-coordinate (m)', fontsize=12)
+
+
+ax.scatter(X,Z,Y, s = 5000000*W, c=scalarMap.to_rgba(W), label='Erosion profile')
+
+max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max()
+Xb = max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + (X.max()+X.min())
+Yb = max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + (Y.max()+Y.min())
+Zb = max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + (Z.max()+Z.min())
+# Comment or uncomment following both lines to test the fake bounding box:
+for xb, yb, zb in zip(Xb, Yb, Zb):
+    ax.plot([xb], [yb], [zb], 'w')
+
+
+#plt.title("Erosion profile (CFD)", fontsize=15)
+scalarMap.set_array(W)
+def fmt(x, pos):
+    if x>0:
+       a, b = '{:.2e}'.format(x).split('e')
+       b = int(b)
+       return r'${} \times 10^{{{}}}$'.format(a, b)
+cbar = plt.colorbar(scalarMap, shrink=0.7, pad=0.2, location='right', format=ticker.FuncFormatter(fmt))
+cbar.ax.tick_params(labelsize=10)
+cbar.ax.set_title('Erosion rate \n (kg/$m^2$·s)',fontsize=10)
+
+
+ax.axes.set_xlim3d(left=-0.8, right=0.8)
+ax.axes.set_ylim3d(bottom=-0.8, top=0.8)
+ax.axes.set_zlim3d(bottom=-0.8, top=0.8)
+# plt.grid()
+
+#plt.tight_layout()
+
+
+#plt.axis('off')
+
+ax.set_xlim(xmin=-1.0, xmax= 0.9)
+ax.set_ylim(ymin=0, ymax= 0.9) 
+ax.set_zlim(zmin=-0.9, zmax= 0) 
+ax.xaxis.set_major_locator(plt.MaxNLocator(5))
+ax.yaxis.set_major_locator(plt.MaxNLocator(5))
+ax.zaxis.set_major_locator(plt.MaxNLocator(5))
+ax.tick_params(axis='both', which='major', labelsize=15)
+
+os.chdir('/bigdata/wonglab/syang159/OP650_1/level_8/new_2')
+plt.savefig('lgfs_plot_original_%i.png'%sim_num)
+
+
---
+++ level_6/predicted_trajectory_plot.py
@@ -0,0 +1,156 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+import glob
+import csv
+from numpy import genfromtxt
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from numpy import array
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+
+Number_of_simulations = 3125
+Number_of_particle = 196
+Number_of_timesteps = 50
+Min_particle_size = 0.0000400
+Particle_size_interval = 0.000002
+Min_main_inlet_speed = 8.45
+Main_inlet_speed_interval = 0.8
+Min_sub_inlet_speed = 25
+Sub_inlet_speed_interval = 4
+Min_main_inlet_pressure = 12000000
+Main_inlet_pressure_interval = 960000
+Min_sub_inlet_pressure = 1000000
+Sub_inlet_pressure_interval = 400000
+Number_of_parameters_for_the_X = 8
+
+
+# read x
+path = ('/bigdata/wonglab/syang159/OP650_1/')
+os.chdir(path + 'level_8/')
+
+
+X = pd.read_csv('X2Prime_denormalized.csv', delimiter=',', index_col=[0])
+X = pd.DataFrame(X).to_numpy()
+X= X.reshape((Number_of_simulations),  Number_of_particle*Number_of_timesteps, 3)
+
+sim_num=670
+X_s = X[sim_num]
+
+X_tr = np.reshape(X_s,(196, 50 ,3), order='F')
+
+X_cut = np.delete(X_tr, np.s_[0:196:2], 0)
+X_cut = np.delete(X_cut, np.s_[0:196:2], 0)
+# X_cut = np.delete(X_cut, np.s_[2:196:2], 1)
+
+
+def data_for_cylinder_main(center_y,center_z,radius,height_x):
+       x = np.linspace(0, height_x, 50)-0.64025
+       theta = np.linspace(0, 2*np.pi, 50)
+       theta_grid, x_grid=np.meshgrid(theta, x)
+       y_grid = radius*np.cos(theta_grid) + center_y
+       z_grid = radius*np.sin(theta_grid) + center_z
+       return x_grid,y_grid,z_grid
+Xm,Ym,Zm = data_for_cylinder_main(0,0,0.269,1.8805)
+def data_for_cylinder_inlet(center_x,center_z,radius,height_y):
+       y = np.linspace(0, -height_y, 50)-0.269
+       theta = np.linspace(0, 2*np.pi, 50)
+       theta_grid, y_grid=np.meshgrid(theta, y)
+       x_grid = radius*np.cos(theta_grid) + center_x
+       z_grid = radius*np.sin(theta_grid) + center_z
+       return x_grid,y_grid,z_grid
+Xi1,Yi1,Zi1 = data_for_cylinder_inlet(-0.5435,0,0.01875,0.269)
+Xi2,Yi2,Zi2 = data_for_cylinder_inlet(0.0,0,0.01875,0.269)
+Xi3,Yi3,Zi3 = data_for_cylinder_inlet(0.5435,0,0.01875,0.269)
+
+fig = plt.figure()
+ax=fig.add_subplot(111,projection='3d')
+ax.plot_surface(Xm,Zm,Ym,alpha=0.1,color='blue')
+ax.plot_surface(Xi1,Zi1,Yi1,alpha=0.1,color='blue')
+ax.plot_surface(Xi2,Zi2,Yi2,alpha=0.1,color='blue')
+ax.plot_surface(Xi3,Zi3,Yi3,alpha=0.1,color='blue')
+ax.xaxis.set_major_locator(plt.MaxNLocator(5))
+ax.yaxis.set_major_locator(plt.MaxNLocator(5))
+ax.zaxis.set_major_locator(plt.MaxNLocator(5))
+
+ax.set_xlabel('X-coordinate (m)', fontsize=12)
+ax.set_ylabel('Y-coordinate (m)', fontsize=12)
+ax.set_zlabel('Z-coordinate (m)', fontsize=12)
+
+for i in range(0,18,1):
+    X = X_cut[i][:,0]
+    Y = X_cut[i][:,1]
+    Z = X_cut[i][:,2]
+
+
+
+    ax.plot(X, Z, Y, color='cyan', linestyle='-',linewidth=1, markersize=2.5)
+
+    max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max()
+    Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(X.max()+X.min())
+    Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Y.max()+Y.min())
+    Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Z.max()+Z.min())
+# Comment or uncomment following both lines to test the fake bounding box:
+    for xb, yb, zb in zip(Xb, Yb, Zb):
+        ax.plot([xb], [yb], [zb], 'w')
+
+for i in range(18,34,1):
+    X = X_cut[i][:,0]
+    Y = X_cut[i][:,1]
+    Z = X_cut[i][:,2]
+
+
+
+    ax.plot(X, Z, Y, color='orange', linestyle='-',linewidth=1, markersize=2.5)
+
+    max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max()
+    Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(X.max()+X.min())
+    Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Y.max()+Y.min())
+    Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Z.max()+Z.min())
+# Comment or uncomment following both lines to test the fake bounding box:
+    for xb, yb, zb in zip(Xb, Yb, Zb):
+        ax.plot([xb], [yb], [zb], 'w')
+
+
+for i in range(34,49,1):
+    X = X_cut[i][:,0]
+    Y = X_cut[i][:,1]
+    Z = X_cut[i][:,2]
+
+
+
+    ax.plot(X, Z, Y, color='magenta', linestyle='-',linewidth=1, markersize=2.5)
+
+    max_range = np.array([X.max()-X.min(), Y.max()-Y.min(), Z.max()-Z.min()]).max()
+    Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(X.max()+X.min())
+    Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Y.max()+Y.min())
+    Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Z.max()+Z.min())
+# Comment or uncomment following both lines to test the fake bounding box:
+    for xb, yb, zb in zip(Xb, Yb, Zb):
+        ax.plot([xb], [yb], [zb], 'w')
+
+# plt.title("Particle trajectory (RNN+CNN) on %ith dataset"%sim_num, fontsize=15)
+
+ax.axes.set_xlim3d(left=-0.8, right=0.8)
+ax.axes.set_ylim3d(bottom=-0.8, top=0.8)
+ax.axes.set_zlim3d(bottom=-0.8, top=0.8)
+# plt.grid()
+# Hide grid lines
+ax.grid(False)
+
+plt.tight_layout()
+# Hide axes ticks
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_zticks([])
+plt.axis('off')
+os.chdir('/bigdata/wonglab/syang159/OP650_1/level_8/new_2')
+plt.savefig('plot_predicted_t_%i.png'%sim_num)
